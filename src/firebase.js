a12b70303a82b7e12fe6db04c151902fe785a4fd
Rahul Rewar <133180470+RAHULREWAR122@users.noreply.github.com>
2024-09-17 08:32:35 +0530

    update firebase file

--- src/firebase.js
+++ src/firebase.js
@@ -5,21 +5,12 @@ import { getFirestore } from "firebase/firestore";
 
 
 const firebaseConfig = {
-<<<<<<< HEAD
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTO_DOMAIN,
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGE_SENDERID,
   appId: process.env.REACT_APP_APP_ID
-=======
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
->>>>>>> ee608d2f82ac244a203f96bba96629610666c85f
 };
 
 // Initialize Firebase
